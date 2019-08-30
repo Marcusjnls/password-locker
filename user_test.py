a656ce8182c12b53107aac048fb71e6d166b4221
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-08-30 16:38:28 +0300

    Add test_save_multiple_contact test case to test if multiple contacts can be saved

--- user_test.py
+++ user_test.py
@@ -1,5 +1,5 @@
 import unittest # Importing the unittest module
-from User import User # Importing the contact class
+from user import User # Importing the contact class
 
 class TestUser(unittest.TestCase):
 
@@ -38,4 +38,16 @@ class TestUser(unittest.TestCase):
         '''
 
         User.user_list = []
+
+    def test_save_multiple_users(self):
+
+        '''
+        Check if we can save multiple user objects to our user_list
+        '''
+        
+        self.new_user.save_user()
+        test_user = User("Luke", "Skywalker", "darthvader", "iamyourfather")
+        test_user.save_user()
+
+        self.assertEqual(len(User.user_list),2)
 
