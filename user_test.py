daf7fbcac4510f5dcd3ff5d37471d0fbef74d0c0
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-08-30 16:16:59 +0300

    Add set up method to test file

--- user_test.py
+++ user_test.py
@@ -1,2 +1,16 @@
 import unittest # Importing the unittest module
 from User import User # Importing the contact class
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+
+        def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Marcus", "Jean-Louis", "marcusjnls", "password1234")
